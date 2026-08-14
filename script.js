17836155350a1e733bceb85f02922492c41d4e79
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-14 08:41:01 +0530

    Add mobile phone data to script.js

---
+++ script.js
@@ -0,0 +1,107 @@
+const mobiles = [
+
+{
+id:1,
+brand:"Apple",
+name:"iPhone 16 Pro Max",
+price:134900,
+storage:"256GB",
+battery:"100%",
+condition:"Brand New",
+color:"Black Titanium",
+warranty:"1 Year",
+badge:"NEW"
+},
+
+{
+id:2,
+brand:"Apple",
+name:"iPhone 15 Pro",
+price:89900,
+storage:"256GB",
+battery:"96%",
+condition:"Used",
+color:"Natural Titanium",
+warranty:"6 Months",
+badge:"USED"
+},
+
+{
+id:3,
+brand:"Samsung",
+name:"Galaxy S25 Ultra",
+price:109999,
+storage:"512GB",
+battery:"100%",
+condition:"Brand New",
+color:"Titanium Gray",
+warranty:"1 Year",
+badge:"NEW"
+},
+
+{
+id:4,
+brand:"Samsung",
+name:"Galaxy S24 Ultra",
+price:74999,
+storage:"256GB",
+battery:"95%",
+condition:"Used",
+color:"Black",
+warranty:"6 Months",
+badge:"USED"
+},
+
+{
+id:5,
+brand:"OnePlus",
+name:"OnePlus 13",
+price:54999,
+storage:"256GB",
+battery:"100%",
+condition:"Brand New",
+color:"Midnight",
+warranty:"1 Year",
+badge:"NEW"
+},
+
+{
+id:6,
+brand:"OnePlus",
+name:"OnePlus 12",
+price:39999,
+storage:"256GB",
+battery:"94%",
+condition:"Used",
+color:"Green",
+warranty:"6 Months",
+badge:"USED"
+},
+
+{
+id:7,
+brand:"Google",
+name:"Pixel 9 Pro",
+price:79999,
+storage:"256GB",
+battery:"100%",
+condition:"Brand New",
+color:"Obsidian",
+warranty:"1 Year",
+badge:"NEW"
+},
+
+{
+id:8,
+brand:"Nothing",
+name:"Nothing Phone 3",
+price:42999,
+storage:"256GB",
+battery:"100%",
+condition:"Brand New",
+color:"White",
+warranty:"1 Year",
+badge:"NEW"
+}
+
+];
