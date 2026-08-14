c7ef5f24256ad692b0231d722023589dc2e997f3
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-14 09:33:46 +0530

    Add images array to mobile phone entries

--- mobiles.js
+++ mobiles.js
@@ -13,7 +13,12 @@ warranty:"1 Year",
 display:"6.9-inch OLED",
 camera:"48MP Triple Camera",
 processor:"A18 Pro",
-badge:"NEW"
+badge:"NEW",
+images:[
+"images/iphone16-1.jpg",
+"images/iphone16-2.jpg",
+"images/iphone16-3.jpg"
+]
 },
 
 {
@@ -29,7 +34,12 @@ warranty:"6 Months",
 display:"6.1-inch OLED",
 camera:"48MP Triple Camera",
 processor:"A17 Pro",
-badge:"USED"
+badge:"USED",
+images:[
+"images/iphone15-1.jpg",
+"images/iphone15-2.jpg",
+"images/iphone15-3.jpg"
+]
 },
 
 {
@@ -45,7 +55,12 @@ warranty:"1 Year",
 display:"6.9-inch AMOLED",
 camera:"200MP Quad Camera",
 processor:"Snapdragon 8 Elite",
-badge:"NEW"
+badge:"NEW",
+images:[
+"images/s25-1.jpg",
+"images/s25-2.jpg",
+"images/s25-3.jpg"
+]
 },
 
 {
@@ -61,7 +76,12 @@ warranty:"6 Months",
 display:"6.8-inch AMOLED",
 camera:"200MP Camera",
 processor:"Snapdragon 8 Gen 3",
-badge:"USED"
+badge:"USED",
+images:[
+"images/s24-1.jpg",
+"images/s24-2.jpg",
+"images/s24-3.jpg"
+]
 },
 
 {
@@ -77,7 +97,12 @@ warranty:"1 Year",
 display:"6.82-inch AMOLED",
 camera:"50MP Triple Camera",
 processor:"Snapdragon 8 Elite",
-badge:"NEW"
+badge:"NEW",
+images:[
+"images/op13-1.jpg",
+"images/op13-2.jpg",
+"images/op13-3.jpg"
+]
 },
 
 {
@@ -93,7 +118,12 @@ warranty:"6 Months",
 display:"6.82-inch AMOLED",
 camera:"50MP Camera",
 processor:"Snapdragon 8 Gen 3",
-badge:"USED"
+badge:"USED",
+images:[
+"images/op12-1.jpg",
+"images/op12-2.jpg",
+"images/op12-3.jpg"
+]
 }
 
 ];
