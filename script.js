ea1a701ea7010e49e3fc174db42289b6802a52b2
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-18 00:05:04 +0530

    Implement automatic slide show functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// STEP 2 - Header Ready
+const slides=document.querySelectorAll(".slide");
+const dots=document.querySelectorAll(".dot");
+
+let current=0;
+
+function showSlide(index){
+
+slides.forEach(s=>s.classList.remove("active"));
+dots.forEach(d=>d.classList.remove("active"));
+
+slides[index].classList.add("active");
+dots[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+current=(current+1)%slides.length;
+showSlide(current);
+
+},3500);
